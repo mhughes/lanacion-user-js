fd93001d42f63231054e2eb67794c856485ced52
mhughes <matias@homehero.org>
2015-09-25 14:21:32 -0300

    versioning

--- paywall.lanacion.user.js
+++ paywall.lanacion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Anti paywall lanacion.com.ar
-// @namespace    https://github.com/mhughes
-// @version      0.1
+// @namespace    https://github.com/mhughes/lanacion-user-js
+// @version      0.2
 // @description  Suprime paywall de lanacion.com.ar
 // @author       mhughes
 // @match        http://*.lanacion.com.ar/*
